f555273e550a5b1b96b8257aeeb67a3d8fc3250b
Bruno Kiyoshi Ynumaru <32544393+brunokiyoshi@users.noreply.github.com>
2026-03-12 20:19:36 -0300

    v0 chat with single interaction

--- src/chat.py
+++ src/chat.py
@@ -1,13 +1,27 @@
-from search import search_prompt
+from search import search_prompt, PROMPT_TEMPLATE
+from langchain_openai import  ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+import sys
+from dotenv import load_dotenv
+
 
 def main():
-    chain = search_prompt()
+    load_dotenv()
+    pergunta = sys.argv[1]
+    print(pergunta)
+    results = search_prompt(pergunta)
+
+    chat = ChatOpenAI(model='gpt-5-nano',temperature=0.77, max_completion_tokens=None,timeout=None,max_retries=5)
+
+    template = PromptTemplate(input_variables=["contexto", "pergunta"], template=PROMPT_TEMPLATE)
+
+    chain = template | chat
 
     if not chain:
         print("Não foi possível iniciar o chat. Verifique os erros de inicialização.")
         return
     
-    pass
+    print(chain.invoke({"pergunta": pergunta, "contexto":results}))
 
 if __name__ == "__main__":
     main()
