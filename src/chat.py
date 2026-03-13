7c1bcafb9be23cdf09075d87c99a7999e33a7e52
Bruno Kiyoshi Ynumaru <32544393+brunokiyoshi@users.noreply.github.com>
2026-03-12 21:28:09 -0300

    refatora para implementar CLI interativa com múltiplas interações

--- src/chat.py
+++ src/chat.py
@@ -1,27 +1,45 @@
 from search import search_prompt, PROMPT_TEMPLATE
-from langchain_openai import  ChatOpenAI
+from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
-import sys
+import cmd
 from dotenv import load_dotenv
 
-
 def main():
+
     load_dotenv()
-    pergunta = sys.argv[1]
-    print(pergunta)
-    results = search_prompt(pergunta)
 
-    chat = ChatOpenAI(model='gpt-5-nano',temperature=0.77, max_completion_tokens=None,timeout=None,max_retries=5)
+    class simpleRagChat(cmd.Cmd):
+        intro = "Faça uma pergunta sobre o documento. Diga 'tchau' para sair."
+        prompt = "pergunta: "
+        chat = ChatOpenAI(
+            model="gpt-5-nano",
+            temperature=0.77,
+            max_completion_tokens=None,
+            timeout=None,
+            max_retries=5,
+        )
 
-    template = PromptTemplate(input_variables=["contexto", "pergunta"], template=PROMPT_TEMPLATE)
+        template = PromptTemplate(
+            input_variables=["contexto", "pergunta"], template=PROMPT_TEMPLATE
+        )
 
-    chain = template | chat
+        chain = template | chat
 
-    if not chain:
-        print("Não foi possível iniciar o chat. Verifique os erros de inicialização.")
-        return
-    
-    print(chain.invoke({"pergunta": pergunta, "contexto":results}))
+        def do_ask(self, question):
+            results = search_prompt(question)
+            response = self.chain.invoke({"pergunta": question, "contexto": results})
+            print(response.content)
+
+
+        def precmd(self, question:str):
+            if question.lower() == "tchau":
+                raise SystemExit
+            return f"ask '{question}'"
+    try:
+        simpleRagChat().cmdloop()
+    except KeyboardInterrupt:
+        print("Saindo do chat.")
+        raise SystemExit
 
 if __name__ == "__main__":
     main()
